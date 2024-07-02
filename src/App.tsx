3c552734fea1b1155c1f9c6d1210504bd95108e1
Quang Thái Huy Nhật <quang.thn@teko.vn>
2024-07-02 13:34:58 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+/***
+ * 1. Thêm button "Like button" vào component App.
+ * - Nội dung của button like button có định dạng như sau: "Like | 100", trong đó 100 là tổng số lượt like.
+ * - Button sẽ có class "like-button".
+ * - Số lượt like ban đầu trong counter là 100.
+ *
+ * 2. Users có thể like bằng cách click vào button:
+ * - Số lượt like sẽ tăng thêm 1
+ * - Button like sẽ có class "liked" ngoài class "like-button" (có thể sử dụng classnames library để thực hiện điều này).
+ * 
+ * 3. Users có thể unlike bằng cách click vào button:
+ * - Số lượt like sẽ giảm đi 1
+ * - Button like sẽ không còn class "liked" nữa.
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h2>Like Button</h2>
+      </div>
+      <style>{`
+          .like-button {
+              font-size: 1rem;
+              padding: 5px 10px;
+              color:  #585858;
+          }
+          .liked {
+              font-weight: bold;
+              color: #1565c0;
+          }
+      `}</style>
+    </>
   );
 }
 
