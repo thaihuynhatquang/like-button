3341b3c5cd3e537f14e0463d9f64363fc55e2fb2
Quang Thái Huy Nhật <quang.thn@teko.vn>
2024-09-30 09:42:29 +0700

    test: 💍 unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, waitFor, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders like button with class like-button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const likeButton = screen.getByText("Like | 100");
+  expect(likeButton).toHaveClass("like-button");
+  expect(likeButton).toBeInTheDocument();
+});
+
+test("like button has class liked when user click", async () => {
+  render(<App />);
+  const likeButton = screen.getByText("Like | 100");
+  await waitFor(() => {
+    likeButton.click();
+  });
+  expect(likeButton).toHaveClass("liked");
+  expect(likeButton.textContent).toBe("Like | 101");
+});
+
+test("like button has not class liked when user click again", async () => {
+  render(<App />);
+  const likeButton = screen.getByText("Like | 100");
+  await waitFor(() => {
+    likeButton.click();
+  });
+  expect(likeButton).toHaveClass("liked");
+
+  await waitFor(() => {
+    likeButton.click();
+  });
+  expect(likeButton).not.toHaveClass("liked");
 });
